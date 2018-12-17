1fa23e36f771e59a746ec03ae40c98716dfdc7f7
duanlian <duanlian1210@163.com>
2018-12-17 05:10:12 -0800

    Add first version longest_common_prefix

--- LeetCode.pri
+++ LeetCode.pri
@@ -3,4 +3,5 @@ SOURCES += \
 
 HEADERS += \
     src/longest_string_without_same.h \
-    $$PWD/src/reverse_int.h
+    $$PWD/src/reverse_int.h \
+    $$PWD/src/longest_common_prefix.h
---
+++ src/longest_common_prefix.h
@@ -0,0 +1,10 @@
+// Go for your dream, Barney
+
+// find the longest string without the same character
+// question: https://leetcode-cn.com/explore/interview/card/bytedance/242/string/1014/
+
+#ifndef LONGEST_COMMON_PREFIX_H
+#define LONGEST_COMMON_PREFIX_H
+
+#endif // LONGEST_COMMON_PREFIX_H
+
